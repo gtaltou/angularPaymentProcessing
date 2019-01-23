c607b3be239b9720c08a4f9dcb099b0667b56255
gtaltou <guy.taltou@gmail.com>
2019-01-23 23:01:29 +0100

    GTA: init + creation of in-memory-data.service.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, { dataEncapsulation: false }
+  ) ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
